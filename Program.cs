19f8ff8a718bbe01fc6cd5f838b841334960abf7
Vulnorn <mixadrbk31@gmail.com>
2024-09-23 20:14:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace NewC_1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberImages = 52;
+            int numberLines;
+            int placesLine = 3;
+            int imagesIncompleteIine;
+
+            numberLines = numberImages / placesLine;
+            imagesIncompleteIine= numberImages % placesLine;
+
+            Console.WriteLine($"Получилось полных {numberLines} радов картин, а в последнем {imagesIncompleteIine}");
+            Console.ReadKey(); 
+        }
+    }
+}
